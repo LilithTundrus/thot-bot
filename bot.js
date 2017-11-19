e44cc2486b0d537ece0f570cde5d86c4960caa0a
Lilith Tundrus <echofox123@gmail.com>
2017-11-19 01:56:02 -0500

    mvp commit

--- bot.js
+++ bot.js
@@ -3,8 +3,12 @@
 const config = require('./config.js');                              // Conifg/auth data
 const ver = '0.0.0022';                                             // Arbitrary version for knowing which bot version is deployed
 const fs = require('fs');
+
 // npm packages
 var Discord = require('discord.io');                                // Discord API wrapper
+var emoji = require('node-emoji');
+const randomWord = require('random-word');
+var randomWords = require('random-words');
 
 var bot = new Discord.Client({                                      // Initialize Discord Bot with config.token
     token: config.discordToken,
@@ -21,7 +25,6 @@ bot.on('ready', function (evt) {                                    // Do some l
     });
 });
 
-
 bot.on('message', function (user, userID, channelID, message, evt) {
     if (message.substring(0, 1) == '%') {                           // Listen for messages that will start with `^`
         var args = message.substring(1).split(' ');
@@ -32,13 +35,58 @@ bot.on('message', function (user, userID, channelID, message, evt) {
         args = args.splice(1);
         switch (cmd) {                                              // Bot needs to know if it will execute a command
             case 'random':                                          // Only command ever needed
-            bot.sendMessage({
-                to: channelID,
-                message: AAAAAAAAAAAA,
-                typing: true
-            });
+                let randomNumberForEmojiMessage = getRandomInt(10, 20);
+                let emojiMessage = randomEmojiSet(randomNumberForEmojiMessage);
+                bot.sendMessage({
+                    to: channelID,
+                    message: emojiMessage,
+                });
                 break;
+            case 'emojify':                                     // This is a stupid thing
+                //turn a string into emoji regional indicators
+                if (message.length < 9 || message.trim().length < 9) {
+                    bot.sendMessage({
+                        to: channelID,
+                        message: 'Please give me a string to emojify',
+                        typing: true
+                    });
+                } else {
+                    let emojifyStr = message.substring(9).toLowerCase();
+                    var emojiString = '';
+                    for (var i = 0; i < emojifyStr.length; i++) {
+                        if (emojifyStr.charAt(i) == ' ') {
+                            //do nothing
+                        } else if (emojifyStr.charAt(i) == 'b') {
+                            emojiString = emojiString + ` :b: `
+                        } else {
+                            emojiString = emojiString + ` :regional_indicator_${emojifyStr.charAt(i)}: `
+                        }
+                    }
+                    console.log(emojiString)
+                    bot.sendMessage({
+                        to: channelID,
+                        message: emojiString,
+                    });
+                }
             // Just add any case commands here
         }
     }
 });
+
+
+
+function randomEmojiSet(numberArg) {
+    let emojiString = '';
+    for (var i = 0; i < numberArg; i++) {
+        emojiString = emojiString + `${randomWords()} :${emoji.random().key}: `;
+    }
+    return emojiString;
+}
+
+/**
+ * Returns a random integer between min (inclusive) and max (inclusive)
+ * Using Math.round() will give you a non-uniform distribution!
+ */
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
